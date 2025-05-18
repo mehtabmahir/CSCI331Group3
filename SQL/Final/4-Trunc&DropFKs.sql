91cd54f7f181efbc55edb72b4e4d3f681f0efc22
Mehtab Mahir <mehtab.mahir@gmail.com>
2025-05-18 18:30:13 -0400

    Update 4-Trunc&DropFKs.sql

--- SQL/Final/4-Trunc&DropFKs.sql
+++ SQL/Final/4-Trunc&DropFKs.sql
@@ -1,106 +1,104 @@
 USE PrestigeCars_3NF
 GO
 
-SET ANSI_NULLS ON
+SET ANSI_NULLS ON;
 GO
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON;
 GO
 
 -- =============================================
 -- Author: Nayem Sarker
--- Create date: 5/14/2025
--- Description:	Truncate Star Schema
+-- Purpose: Truncate all tables in key schemas after dropping FKs
 -- =============================================
 
-DROP PROCEDURE IF EXISTS [Project2.5].[TruncateStarSchemaData]
-GO 
+-- Drop the Truncate Procedure if it exists
+DROP PROCEDURE IF EXISTS [Project2.5].[TruncateStarSchemaData];
+GO
 
+-- Recreate Truncate Procedure
 CREATE PROCEDURE [Project2.5].[TruncateStarSchemaData]
-	@UserAuthorizationKey int
-AS 
-BEGIN 
-	SET NOCOUNT ON
-	DECLARE TableCursor CURSOR FOR
-	SELECT DISTINCT 
-		'[' + TABLE_SCHEMA + '].[' + TABLE_NAME + ']' as FullyQualifiedTableName
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE TABLE_TYPE = 'Base Table' AND TABLE_SCHEMA LIKE '%Project3%'
+    @UserAuthorizationKey INT
+AS
+BEGIN
+    SET NOCOUNT ON;
 
-	OPEN TableCursor
-	DECLARE @TableName NVARCHAR(255)
-	DECLARE @SQL VARCHAR(MAX)
+    DECLARE TableCursor CURSOR FOR
+        SELECT DISTINCT '[' + TABLE_SCHEMA + '].[' + TABLE_NAME + ']' AS FullyQualifiedTableName
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_TYPE = 'BASE TABLE'
+          AND TABLE_SCHEMA IN ('Data', 'Reference', 'DataTransfer', 'SourceData', 'Output'); -- updated schema list
 
-	FETCH NEXT FROM TableCursor INTO @TableName
+    OPEN TableCursor;
 
-	WHILE @@FETCH_STATUS = 0
-	BEGIN 
-		SET @SQL = 'TRUNCATE TABLE ' + @TableName
-		EXEC(@SQL)
+    DECLARE @TableName NVARCHAR(255);
+    DECLARE @SQL NVARCHAR(MAX);
 
-		FETCH NEXT FROM TableCursor INTO @TableName
-	END
+    FETCH NEXT FROM TableCursor INTO @TableName;
 
-	CLOSE TableCursor
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @SQL = 'TRUNCATE TABLE ' + @TableName;
+        EXEC(@SQL);
 
-	DEALLOCATE TableCursor
+        FETCH NEXT FROM TableCursor INTO @TableName;
+    END
 
-	EXEC [Process].[usp_TrackWorkFlow]
-        @WorkFlowStepDescription = 'Truncate Star Schema Data.',
-        @UserAuthorizationKey = @UserAuthorizationKey,
-        @WorkFlowStepTableRowCount = -1;
-END
+    CLOSE TableCursor;
+    DEALLOCATE TableCursor;
+
+    EXEC [Process].[usp_TrackWorkFlow]
+         @WorkFlowStepDescription = 'Truncate Star Schema Data.',
+         @UserAuthorizationKey = @UserAuthorizationKey,
+         @WorkFlowStepTableRowCount = -1;
+END;
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
 -- =============================================
 -- Author: Nayem Sarker
--- Create date: 5/14/2025
--- Description:	Drop the Foreign Keys From the Star Schema
+-- Purpose: Drop all foreign keys in the 3NF database
 -- =============================================
 
-DROP PROCEDURE IF EXISTS [Project2.5].[DropForeignKeysFromStarSchemaData]
-
+-- Drop the Drop-FKs Procedure if it exists
+DROP PROCEDURE IF EXISTS [Project2.5].[DropForeignKeysFromStarSchemaData];
 GO
 
+-- Recreate Drop-FKs Procedure
 CREATE PROCEDURE [Project2.5].[DropForeignKeysFromStarSchemaData]
     @UserAuthorizationKey INT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    Declare @ForeignKeyName VARCHAR(255)
-    DECLARE @SQL VARCHAR(MAX)
-    DECLARE @TableName VARCHAR(255)
+    DECLARE @ForeignKeyName VARCHAR(255);
+    DECLARE @TableName NVARCHAR(255);
+    DECLARE @SQL NVARCHAR(MAX);
+
     DECLARE ForeignKeyCursor CURSOR FOR 
-    
-    SELECT fk.name as ForeignKeyName,
-        QUOTENAME(OBJECT_SCHEMA_NAME(fk.parent_object_id)) + '.'+t.name as TableName
-    FROM sys.foreign_keys as fk
-    INNER JOIN sys.tables as t on fk.parent_object_id = t.object_id
+        SELECT fk.name AS ForeignKeyName,
+               QUOTENAME(OBJECT_SCHEMA_NAME(fk.parent_object_id)) + '.' + QUOTENAME(OBJECT_NAME(fk.parent_object_id)) AS TableName
+        FROM sys.foreign_keys AS fk
+        INNER JOIN sys.tables AS t 
+            ON fk.parent_object_id = t.object_id
+        WHERE OBJECT_SCHEMA_NAME(fk.parent_object_id) IN ('Data', 'Reference', 'DataTransfer', 'SourceData', 'Output');  -- match same schema filter
 
-    OPEN ForeignKeyCursor
-
-    FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName
+    OPEN ForeignKeyCursor;
+    FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName;
 
     WHILE @@FETCH_STATUS = 0 
     BEGIN
-        SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT IF EXISTS ' + @ForeignKeyName + ';'
-        EXEC(@SQL)
-        
-        FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName
+        SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT ' + QUOTENAME(@ForeignKeyName) + ';';
+        EXEC(@SQL);
+
+        FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName;
     END
 
-    CLOSE ForeignKeyCursor
-    DEALLOCATE ForeignKeyCursor
+    CLOSE ForeignKeyCursor;
+    DEALLOCATE ForeignKeyCursor;
 
     EXEC [Process].[usp_TrackWorkFlow]
-        @WorkFlowStepDescription = 'Drop Foreign Keys.',
-        @UserAuthorizationKey = @UserAuthorizationKey,
-        @WorkFlowStepTableRowCount = -1;
-
+         @WorkFlowStepDescription = 'Drop Foreign Keys.',
+         @UserAuthorizationKey = @UserAuthorizationKey,
+         @WorkFlowStepTableRowCount = -1;
 END;
 GO
