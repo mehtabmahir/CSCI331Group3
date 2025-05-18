152146fe26018a1e6c78b1b8d6bedad9f692b100
Mehtab Mahir <mehtab.mahir@gmail.com>
2025-05-18 18:28:02 -0400

    Update 4-Trunc&DropFKs.sql

--- SQL/Final/4-Trunc&DropFKs.sql
+++ SQL/Final/4-Trunc&DropFKs.sql
@@ -1,89 +1,106 @@
 USE PrestigeCars_3NF
 GO
 
-SET ANSI_NULLS ON;
+SET ANSI_NULLS ON
 GO
-SET QUOTED_IDENTIFIER ON;
+SET QUOTED_IDENTIFIER ON
 GO
 
 -- =============================================
 -- Author: Nayem Sarker
 -- Create date: 5/14/2025
--- Description: Truncate Star Schema Data (all tables in schemas like 'Project3%')
+-- Description:	Truncate Star Schema
 -- =============================================
-DROP PROCEDURE IF EXISTS [Project2.5].[TruncateStarSchemaData];
-GO
+
+DROP PROCEDURE IF EXISTS [Project2.5].[TruncateStarSchemaData]
+GO 
+
 CREATE PROCEDURE [Project2.5].[TruncateStarSchemaData]
-    @UserAuthorizationKey INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-    DECLARE TableCursor CURSOR FOR
-        SELECT DISTINCT '[' + TABLE_SCHEMA + '].[' + TABLE_NAME + ']' as FullyQualifiedTableName
-        FROM INFORMATION_SCHEMA.TABLES
-        WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA LIKE 'Project3%';  -- adjust schema filter as needed
+	@UserAuthorizationKey int
+AS 
+BEGIN 
+	SET NOCOUNT ON
+	DECLARE TableCursor CURSOR FOR
+	SELECT DISTINCT 
+		'[' + TABLE_SCHEMA + '].[' + TABLE_NAME + ']' as FullyQualifiedTableName
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_TYPE = 'Base Table' AND TABLE_SCHEMA LIKE '%Project3%'
 
-    OPEN TableCursor;
-    DECLARE @TableName NVARCHAR(255);
-    DECLARE @SQL NVARCHAR(MAX);
+	OPEN TableCursor
+	DECLARE @TableName NVARCHAR(255)
+	DECLARE @SQL VARCHAR(MAX)
 
-    FETCH NEXT FROM TableCursor INTO @TableName;
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        SET @SQL = 'TRUNCATE TABLE ' + @TableName;
-        EXEC(@SQL);
-        FETCH NEXT FROM TableCursor INTO @TableName;
-    END
+	FETCH NEXT FROM TableCursor INTO @TableName
 
-    CLOSE TableCursor;
-    DEALLOCATE TableCursor;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN 
+		SET @SQL = 'TRUNCATE TABLE ' + @TableName
+		EXEC(@SQL)
 
-    -- Log workflow step (requires Process.usp_TrackWorkFlow)
-    EXEC [Process].[usp_TrackWorkFlow]
-         @WorkFlowStepDescription = 'Truncate Star Schema Data.',
-         @UserAuthorizationKey = @UserAuthorizationKey,
-         @WorkFlowStepTableRowCount = -1;
-END;
+		FETCH NEXT FROM TableCursor INTO @TableName
+	END
+
+	CLOSE TableCursor
+
+	DEALLOCATE TableCursor
+
+	EXEC [Process].[usp_TrackWorkFlow]
+        @WorkFlowStepDescription = 'Truncate Star Schema Data.',
+        @UserAuthorizationKey = @UserAuthorizationKey,
+        @WorkFlowStepTableRowCount = -1;
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 
 -- =============================================
 -- Author: Nayem Sarker
 -- Create date: 5/14/2025
--- Description: Drop Foreign Keys from the Star Schema
+-- Description:	Drop the Foreign Keys From the Star Schema
 -- =============================================
-DROP PROCEDURE IF EXISTS [Project2.5].[DropForeignKeysFromStarSchemaData];
+
+DROP PROCEDURE IF EXISTS [Project2.5].[DropForeignKeysFromStarSchemaData]
+
 GO
+
 CREATE PROCEDURE [Project2.5].[DropForeignKeysFromStarSchemaData]
     @UserAuthorizationKey INT
 AS
 BEGIN
     SET NOCOUNT ON;
-    DECLARE @ForeignKeyName VARCHAR(255);
-    DECLARE @TableName NVARCHAR(255);
-    DECLARE @SQL NVARCHAR(MAX);
-    DECLARE ForeignKeyCursor CURSOR FOR 
-        SELECT fk.name as ForeignKeyName,
-               QUOTENAME(OBJECT_SCHEMA_NAME(fk.parent_object_id)) + '.' + t.name as TableName
-        FROM sys.foreign_keys AS fk
-        INNER JOIN sys.tables AS t 
-            ON fk.parent_object_id = t.object_id;
 
-    OPEN ForeignKeyCursor;
-    FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName;
+    Declare @ForeignKeyName VARCHAR(255)
+    DECLARE @SQL VARCHAR(MAX)
+    DECLARE @TableName VARCHAR(255)
+    DECLARE ForeignKeyCursor CURSOR FOR 
+    
+    SELECT fk.name as ForeignKeyName,
+        QUOTENAME(OBJECT_SCHEMA_NAME(fk.parent_object_id)) + '.'+t.name as TableName
+    FROM sys.foreign_keys as fk
+    INNER JOIN sys.tables as t on fk.parent_object_id = t.object_id
+
+    OPEN ForeignKeyCursor
+
+    FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName
+
     WHILE @@FETCH_STATUS = 0 
     BEGIN
-        SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT ' + QUOTENAME(@ForeignKeyName) + ';';
-        EXEC(@SQL);
-        FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName;
+        SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT IF EXISTS ' + @ForeignKeyName + ';'
+        EXEC(@SQL)
+        
+        FETCH NEXT FROM ForeignKeyCursor INTO @ForeignKeyName, @TableName
     END
 
-    CLOSE ForeignKeyCursor;
-    DEALLOCATE ForeignKeyCursor;
+    CLOSE ForeignKeyCursor
+    DEALLOCATE ForeignKeyCursor
 
-    -- Log workflow step
     EXEC [Process].[usp_TrackWorkFlow]
-         @WorkFlowStepDescription = 'Drop Foreign Keys.',
-         @UserAuthorizationKey = @UserAuthorizationKey,
-         @WorkFlowStepTableRowCount = -1;
+        @WorkFlowStepDescription = 'Drop Foreign Keys.',
+        @UserAuthorizationKey = @UserAuthorizationKey,
+        @WorkFlowStepTableRowCount = -1;
+
 END;
 GO
